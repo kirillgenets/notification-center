c7455581f36f978e5d025c85afe45261514c7def
kirillgenets <kirill.genets2002@mail.ru>
2019-12-10 10:13:37 +0300

    created Filter component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './Filter.css';
+
+const Filter = (props) => {
+  return (
+    props.isVisible ?
+      <form className={styles.filter}>
+        <button className={styles.reset}>Reset Filter</button>
+        <fieldset className={styles.group}>
+          <div className={styles.row}>
+            <input className={styles.radio} id="read-radio" type="radio" name="read/unread" />
+            <label className={styles.label} htmlFor="read-radio">Read</label>
+          </div>
+          <div className={styles.row}>
+            <input className={styles.radio} id="unread-radio" type="radio" name="read/unread" />
+            <label className={styles.label} htmlFor="unread-radio">Unread</label>
+          </div>
+        </fieldset>
+        <fieldset className={styles.group}>
+          <div className={styles.row}>
+            <input className={styles.radio} id="critical-radio" type="radio" name="category" />
+            <label className={styles.label} htmlFor="critical-radio">Critical</label>
+          </div>
+          <div className={styles.row}>
+            <input className={styles.radio} id="warn-radio" type="radio" name="category" />
+            <label className={styles.label} htmlFor="warn-radio">Warn</label>
+          </div>
+          <div className={styles.row}>
+            <input className={styles.radio} id="success-radio" type="radio" name="category" />
+            <label className={styles.label} htmlFor="success-radio">Success</label>
+          </div>
+          <div className={styles.row}>
+            <input className={styles.radio} id="info-radio" type="radio" name="category" />
+            <label className={styles.label} htmlFor="info-radio">Info</label>
+          </div>
+        </fieldset>
+        <button className={styles.submit}>Apply</button>
+      </form> :
+      null
+  )
+}
+
+export default Filter;
--- src/components/HeadingColumn/HeadingColumn.jsx
+++ src/components/HeadingColumn/HeadingColumn.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Filter from '../Filter/Filter';
 import styles from './HeadingColumn.css';
 
 const HeadingColumn = (props) => {
   return (
     <td className={styles.column}>
-      {props.isSubmenu ? 
-        <button className={styles.toggler}>{props.name}</button> :
+      {props.isSubmenu ?
+        <React.Fragment>
+          <button className={styles.toggler}>{props.name}</button>
+          <Filter isVisible={true} />
+        </React.Fragment> :
         <span className={styles.usual}>{props.name}</span>
       }
     </td>
