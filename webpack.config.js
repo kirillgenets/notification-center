21b50ff30f23ef239f1bddd5b109492ba2f84c46
kirillgenets <kirill.genets2002@mail.ru>
2019-12-18 22:03:25 +0300

    changed tab size in wepack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,59 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js$|jsx$)/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            },
-            {
-                test: /\.css/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]',
-                                context: path.resolve(__dirname, 'src'),
-                                hashPrefix: 'my-custom-hash'
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|svg|gif)/,
-                use: [
-                    'file-loader'
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js$|jsx$)/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]',
+                context: path.resolve(__dirname, 'src'),
+                hashPrefix: 'my-custom-hash'
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)/,
+        use: [
+          'file-loader'
+        ]
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
